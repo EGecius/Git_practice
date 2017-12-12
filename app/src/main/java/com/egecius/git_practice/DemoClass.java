a4c27b21a4a7a645692c4f49f0b3c871cbee402a
Egidijus Gecius <e.gecius@gmail.com>
2017-12-12 08:26:37 +0000

    add 2 strings

--- app/src/main/java/com/egecius/git_practice/DemoClass.java
+++ app/src/main/java/com/egecius/git_practice/DemoClass.java
@@ -5,5 +5,6 @@ public class DemoClass {
     String string1;
     String string2;
     String string3;
+    String string4;
 
 }
--- app/src/main/java/com/egecius/git_practice/DemoClass2.java
+++ app/src/main/java/com/egecius/git_practice/DemoClass2.java
@@ -2,5 +2,6 @@ package com.egecius.git_practice;
 
 public class DemoClass2 {
 
+    String string1;
 
 }
