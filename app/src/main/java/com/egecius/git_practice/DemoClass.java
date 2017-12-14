724b219614578f6e1339e960e031a46b65e36eed
Egidijus Gecius <e.gecius@gmail.com>
2017-12-14 07:12:15 +0000

    Commented out

--- app/src/main/java/com/egecius/git_practice/DemoClass.java
+++ app/src/main/java/com/egecius/git_practice/DemoClass.java
@@ -2,7 +2,7 @@ package com.egecius.git_practice;
 
 public class DemoClass {
 
-    String string1;
+//    String string1;
 //    String string2;
 //    String string3;
 
--- app/src/main/java/com/egecius/git_practice/DemoClass2.java
+++ app/src/main/java/com/egecius/git_practice/DemoClass2.java
@@ -2,8 +2,8 @@ package com.egecius.git_practice;
 
 public class DemoClass2 {
 
-    String string1;
+//    String string1;
 
-    
+//
 
 }
