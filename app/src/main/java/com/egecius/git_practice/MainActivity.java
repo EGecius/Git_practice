e04356726720137eba62a0618e47e7f6c74ee18b
Egidijus Gecius <>
2018-03-16 10:08:07 +0000

    remove redundant

--- app/src/main/java/com/egecius/git_practice/MainActivity.java
+++ app/src/main/java/com/egecius/git_practice/MainActivity.java
@@ -2,26 +2,13 @@ package com.egecius.git_practice;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-    }
-
-
-    void demo() {
-        try {
-        	throw new RuntimeException("Eg");
-        } catch (Exception e) {
-        	e.printStackTrace();
-        }
-        Log.v("Eg:MainActivity:21", "demo added in 'demo_2'");
-
-
+        setContentView(R.layout.activity_main);
     }
     
 }
